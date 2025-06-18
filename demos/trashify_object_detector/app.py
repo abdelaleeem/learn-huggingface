29ddd9e5bf113272ea8a6cf365762081f8268318
mrdbourke <danielbourke@icloud.com>
2025-06-18 12:02:33 +1000

    update readme + demos + OD notebook

--- demos/trashify_object_detector/app.py
+++ demos/trashify_object_detector/app.py
@@ -161,7 +161,7 @@ demo = gr.Interface(
         gr.Text(label="Text Output")
     ],
     title="🚮 Trashify Object Detection Demo V4",
-    description="Help clean up your local area! Upload an image and get +1 if there is all of the following items detected: trash, bin, hand.",
+    description=description,
     # Examples come in the form of a list of lists, where each inner list contains elements to prefill the `inputs` parameter with
     # See where the examples originate from here: https://huggingface.co/datasets/mrdbourke/trashify_examples/
     examples=[
